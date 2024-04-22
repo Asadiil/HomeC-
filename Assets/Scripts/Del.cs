bfde023ee016173d835f223cd97e851f81f1756c
Dmitry Dolmatov <dima_dolmatov@mail.ru>
2024-04-22 22:56:43 +0700

    One

---
+++ Assets/Scripts/Del.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Del : MonoBehaviour
+{
+    public Text FirstNum;
+    public Text SecondNum;
+    public Text OtvetNum;
+
+    public void onDel()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Proizv.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Proizv : MonoBehaviour
+{
+    public Text FirstNum;
+    public Text SecondNum;
+    public Text OtvetNum;
+
+    public void onProizv()
+    {
+        
+    }
+
+}
